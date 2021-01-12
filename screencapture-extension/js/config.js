1d1087fd4e97e1b4c7e55d0c1d1a2d91ee3ed10f
yarik-brit <72208403+yarik-brit@users.noreply.github.com>
2021-01-12 14:37:19 +0200

    fix for server

--- screencapture-extension/js/config.js
+++ screencapture-extension/js/config.js
@@ -1,5 +1,5 @@
 var config = {
     timerCd : 20,
-    base_url : "https://localhost:44336/"
-    // base_url : "http://tensionx-002-site8.btempurl.com/"
+    // base_url : "https://localhost:44336/"
+    base_url : "http://tensionx-002-site8.btempurl.com/"
 };
