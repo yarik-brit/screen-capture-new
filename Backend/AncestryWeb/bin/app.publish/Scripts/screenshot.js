01c6e739a3fdad7d4ddb2df437806e5796938c31
yarik-brit <72208403+yarik-brit@users.noreply.github.com>
2021-01-21 13:47:00 +0200

    latest

---
+++ Backend/AncestryWeb/bin/app.publish/Scripts/screenshot.js
@@ -0,0 +1,43 @@
+﻿window.onload = init;
+
+var baseUrl = "https://testscreenshot.gear.host";
+//var baseUrl = "https://localhost:44336";
+
+async function init() {
+    if (document.location.pathname == "/Screenshot" && document.location.search.split(/[?=]+/)[1] == "id") {
+        if (document.querySelector('.name > h2') != null) {
+            document.querySelector('.btn-copy').onclick = () => {
+                navigator.clipboard.writeText(baseUrl + document.location.pathname + document.location.search);
+            };
+            document.querySelector('.btn-delete').onclick = async function () {
+                deleteImg().then(result => location.replace(baseUrl + "/Screenshot?id=" + result))
+                    .catch(error => location.replace(baseUrl + "/Screenshot?id=" + result))
+            };
+        }
+    }
+}
+
+
+function deleteImg() {
+    var formData = new FormData();
+    formData.append("imgName", document.querySelector(".name > h2").textContent);
+
+    return new Promise((resolve, reject) =>
+        $.ajax({
+            type: "POST",
+            url: `${baseUrl}/Screenshot/DeleteImageByName`,
+            cache: false,
+            processData: false,
+            contentType: false,
+            //dataType: "json",
+            data: formData,
+            success: (result) => {
+                resolve(result);
+            },
+            error: (result) => {
+                console.log(result);
+                reject(result);
+            },
+        })
+    )
+}
